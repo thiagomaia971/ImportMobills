7d0d9e6690ce3e9f13e1cb01b1cb31073610c96b
Thiago Maia <thiago.maia971@gmail.com>
2024-11-02 23:38:12 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from csv_converter import CSVConverter
 
 # Exemplo de uso
-input_path = 'faturas/input/Fatura_2024-10-05.csv'
-output_path = 'faturas/output/Fatura_2024-10-05.csv'
+input_path = 'faturas/input/Fatura_2024-11-05.csv'
+output_path = 'faturas/output/Fatura_2024-11-05.csv'
 config_path = 'config.json'
 
 converter = CSVConverter(input_path, output_path, config_path)
